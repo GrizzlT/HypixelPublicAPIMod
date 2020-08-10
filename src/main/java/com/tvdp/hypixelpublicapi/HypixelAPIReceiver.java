a7320c03270a33a9986dd8591137c40322a2b66e
ThomasVDP <T1@terinfo.net>
2020-08-10 21:28:43 +0200

    Added two-way requests!

---
+++ src/main/java/com/tvdp/hypixelpublicapi/HypixelAPIReceiver.java
@@ -0,0 +1,6 @@
+package com.tvdp.hypixelpublicapi;
+
+public interface HypixelAPIReceiver
+{
+    void onReceivePublicAPIManager(HypixelPublicAPIModLibrary instance);
+}
--- src/main/java/com/tvdp/hypixelpublicapi/event/OnHpPublicAPIReadyEvent.java
+++ src/main/java/com/tvdp/hypixelpublicapi/event/OnHpPublicAPIReadyEvent.java
@@ -1,14 +1,20 @@
 package com.tvdp.hypixelpublicapi.event;
 
-import com.tvdp.hypixelpublicapi.HypixelPublicAPIModLibrary;
+import com.tvdp.hypixelpublicapi.HypixelAPIReceiver;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
+/**
+ * This event must be triggered by the mod that will use the api manager
+ */
 public class OnHpPublicAPIReadyEvent extends Event
 {
-    public final HypixelPublicAPIModLibrary apiLibrary;
+    public final HypixelAPIReceiver receiver;
 
-    public OnHpPublicAPIReadyEvent(HypixelPublicAPIModLibrary apiLibrary)
+    /**
+     * @param receiver the class that will manage storage of the api manager
+     */
+    public OnHpPublicAPIReadyEvent(HypixelAPIReceiver receiver)
     {
-        this.apiLibrary = apiLibrary;
+        this.receiver = receiver;
     }
 }
