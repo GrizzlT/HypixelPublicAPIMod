625fb17ad0df43526edb240e0c8e5c0fd4428aa8
ThomasVDP <T1@terinfo.net>
2020-08-12 21:51:03 +0200

    Added NoAPIKey exception

--- src/main/java/com/tvdp/hypixelpublicapi/HypixelPublicAPIModLibrary.java
+++ src/main/java/com/tvdp/hypixelpublicapi/HypixelPublicAPIModLibrary.java
@@ -1,10 +1,11 @@
 package com.tvdp.hypixelpublicapi;
 
+import com.tvdp.hypixelpublicapi.error.PublicAPIKeyMissingException;
 import net.hypixel.api.HypixelAPI;
 
 import java.util.function.Function;
 
 public interface HypixelPublicAPIModLibrary
 {
-    <T> void handleHypixelAPIRequest(Function<HypixelAPI, T> requestFunc);
+    <T> void handleHypixelAPIRequest(Function<HypixelAPI, T> requestFunc) throws PublicAPIKeyMissingException;
 }
---
+++ src/main/java/com/tvdp/hypixelpublicapi/error/PublicAPIKeyMissingException.java
@@ -0,0 +1,5 @@
+package com.tvdp.hypixelpublicapi.error;
+
+public class PublicAPIKeyMissingException extends Exception
+{
+}
