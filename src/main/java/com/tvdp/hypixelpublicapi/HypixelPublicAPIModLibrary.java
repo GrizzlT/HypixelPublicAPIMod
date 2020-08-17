af9cfbe5a70208ca7abffde7837cf0bc36ef3635
ThomasVDP <T1@terinfo.net>
2020-08-17 17:58:34 +0200

    Updated HypixelPublicAPIModLibrary

--- src/main/java/com/tvdp/hypixelpublicapi/HypixelPublicAPIModLibrary.java
+++ src/main/java/com/tvdp/hypixelpublicapi/HypixelPublicAPIModLibrary.java
@@ -3,9 +3,10 @@ package com.tvdp.hypixelpublicapi;
 import com.tvdp.hypixelpublicapi.error.PublicAPIKeyMissingException;
 import net.hypixel.api.HypixelAPI;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 public interface HypixelPublicAPIModLibrary
 {
-    <T> T handleHypixelAPIRequest(Function<HypixelAPI, T> requestFunc) throws PublicAPIKeyMissingException;
+    <T extends CompletableFuture> T handleHypixelAPIRequest(Function<HypixelAPI, T> requestFunc) throws PublicAPIKeyMissingException;
 }
