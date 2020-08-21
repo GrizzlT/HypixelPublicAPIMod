5971e202683673ace9fd70ea69a8943156a5ab55
ThomasVDP <T1@terinfo.net>
2020-08-21 20:34:01 +0200

    Changed to Tascalate Concurrent library!

--- src/main/java/com/tvdp/hypixelpublicapi/HypixelPublicAPIModLibrary.java
+++ src/main/java/com/tvdp/hypixelpublicapi/HypixelPublicAPIModLibrary.java
@@ -2,11 +2,12 @@ package com.tvdp.hypixelpublicapi;
 
 import com.tvdp.hypixelpublicapi.error.PublicAPIKeyMissingException;
 import net.hypixel.api.HypixelAPI;
+import net.tascalate.concurrent.Promise;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 public interface HypixelPublicAPIModLibrary
 {
-    <T> CompletableFuture<T> handleHypixelAPIRequest(Function<HypixelAPI, CompletableFuture<T>> requestFunc) throws PublicAPIKeyMissingException;
+    <T> Promise<T> handleHypixelAPIRequest(Function<HypixelAPI, CompletableFuture<T>> apiRequestFunc) throws PublicAPIKeyMissingException;
 }
