07a1b43cf721ba127d8889c33b39f407638e7e43
ThomasVDP <T1@terinfo.net>
2020-12-19 22:04:53 +0100

    Forced to return AbstractReply

--- src/main/java/com/github/grizzlt/hypixelpublicapi/HypixelPublicAPIModLibrary.java
+++ src/main/java/com/github/grizzlt/hypixelpublicapi/HypixelPublicAPIModLibrary.java
@@ -2,6 +2,7 @@ package com.github.grizzlt.hypixelpublicapi;
 
 import com.github.grizzlt.hypixelpublicapi.error.PublicAPIKeyMissingException;
 import net.hypixel.api.HypixelAPI;
+import net.hypixel.api.reply.AbstractReply;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.CompletableFuture;
@@ -9,5 +10,5 @@ import java.util.function.Function;
 
 public interface HypixelPublicAPIModLibrary
 {
-    <T> Mono<T> handleHypixelAPIRequest(Function<HypixelAPI, CompletableFuture<T>> apiRequestFunc) throws PublicAPIKeyMissingException;
+    <T extends AbstractReply> Mono<T> handleHypixelAPIRequest(Function<HypixelAPI, CompletableFuture<T>> apiRequestFunc) throws PublicAPIKeyMissingException;
 }
