63fc64e0f7087457f35c1305030a71d31e4b89a6
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2021-09-07 10:04:59 +0200

    Changed hardcoded version to 2.2.0

--- src/main/java/com/github/grizzlt/hypixelapimod/Reference.java
+++ src/main/java/com/github/grizzlt/hypixelapimod/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
     public static final String MOD_ID = "hypixelapimod";
     public static final String MOD_NAME = "Hypixel PublicAPI Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.2.0";
     public static final String GUI_FACTORY = "com.github.grizzlt.hypixelapimod.config.ConfigGuiFactory";
 }
